817986cfeec2dc1a6743a68c25c5ba5e7f8c517a
Maria Manoela <manoelamvc@hotmail.com>
2021-12-22 19:46:14 -0300

    Create objects and implement methods in main class

--- src/aula01/Main.java
+++ src/aula01/Main.java
@@ -1,4 +1,14 @@
 package aula01;
 
 public class Main {
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumeroConta(123);
+        p1.setDono("Manu");
+        p1.estadoAtual();
+        p1.abrirConta("CP");
+        p1.depositar(400);
+        p1.sacar(100);
+        p1.estadoAtual();
+    }
 }
