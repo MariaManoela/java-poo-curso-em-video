6aa9a882a822b995d360b576e7625db0d0477b53
Maria Manoela <manoelamvc@hotmail.com>
2021-12-22 15:13:51 -0300

    Add class attributes

---
+++ src/aula01/ContaBanco.java
@@ -0,0 +1,11 @@
+package aula01;
+
+public class ContaBanco {
+    private int numeroConta;
+    private String tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
+
+
+}
