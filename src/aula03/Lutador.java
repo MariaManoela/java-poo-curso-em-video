171a011bd57b6d56321348bc560484d28160c19f
Maria Manoela <manoelamvc@hotmail.com>
2021-12-23 11:28:33 -0300

    Create constructor

--- src/aula03/Lutador.java
+++ src/aula03/Lutador.java
@@ -10,4 +10,15 @@ public class Lutador {
     private int vitorias;
     private int derrotas;
     private int empates;
+
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
 }
